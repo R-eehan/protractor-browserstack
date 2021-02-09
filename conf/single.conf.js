f247c512db61bef21d919ac09075de5711e26b66
Reehan <reehan@Reehans-MacBook-Pro.local>
2021-02-09 21:09:21 +0530

    adding acceptSslCerts cap to bypass cert error

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -6,14 +6,14 @@ exports.config = {
     'browserstack.local': process.env.BROWSERSTACK_LOCAL,
     'browserstack.localIdentifier': process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
     'build': process.env.BROWSERSTACK_BUILD_NAME,
-    'name': 'no acceptSslCert',
+    'name': 'With acceptSslCerts',
     'browserName': 'Chrome',
     'browser_version': 'latest',
     'os': 'OS X',
     'os_version': 'Catalina',
     'resolution': '1024x768',
     'browserstack.debug': 'true',
-    // 'browserstack.networkLogs': 'true',
+    'browserstack.networkLogs': 'true',
     'browserstack.console': 'verbose'
   }
 };
